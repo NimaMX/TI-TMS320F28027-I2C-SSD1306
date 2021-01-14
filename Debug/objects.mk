b6392d0cb9bacb47c12dd680e912da0fde865f9a
Nima Mohammadi <nima__mohammadi@outlook.com>
2021-01-15 00:27:54 +0100

    Init project structure, adding ccs 10 setting

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"C:/ti/c2000/C2000Ware_3_03_00_00/device_support/f2802x/common/ccs/Debug/driverlib.lib" -l"C:/ti/c2000/C2000Ware_3_03_00_00/device_support/f2802x/common/cmd/F2802x_generic_flash.cmd"
+
